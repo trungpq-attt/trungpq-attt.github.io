e88ed53c62bdf56ace823665b1b14c25bf375464
TrungPQ <trungpq.attt@gmail.com>
2026-07-27 22:08:23 +0700

    add deploy workflow

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 export default defineConfig({
 	site: siteConfig.url,
 	image: {
-		domains: ["webmention.io"],
+		domains: ["trungpq-attt.github.io"],
 	},
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
